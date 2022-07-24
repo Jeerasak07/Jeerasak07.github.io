0aec2609ab790ca69ce58aec69bb6132fac0f18e
Jeerasak Phalee <76503403+Jeerasak07@users.noreply.github.com>
2022-07-24 12:46:14 +0700

    create

--- Todolist/indexdom.js
+++ Todolist/indexdom.js
@@ -1,45 +1,28 @@
-let h1 = document.getElementById('title');
-let h2 = document.getElementsByTagName('h1');
-let h3 = document.getElementsByClassName('display-3');
-
-function changetext(id){
-    console.log(id);
-    id.innerHTML = "This text change from JS";
-}
-
-function tempCalculator(){
-    var c = document.getElementById('inputCelsius').value
-    var f = document.getElementById('inputFahrenheit').value
-    console.log(c, f);
-}
-document.getElementById('calbtn').addEventListener("click", tempCalculator);
-
 function addTodo(){
-    let input = document.getElementById('inputtodo').value;
-    let newli = document.createElement('li');
-    let node = document.createTextNode(input);
-    
-    newli.className = "list-group-item";
-    newli.appendChild(node);
+  let input = document.getElementById('inputtodo').value;
+  let newli = document.createElement('li');
+  let node = document.createTextNode(input);
+  
+  newli.className = "list-group-item";
+  newli.appendChild(node);
 
-    let elist = document.getElementById('todolist');
-    elist.appendChild(newli);
+  let elist = document.getElementById('todolist');
+  elist.appendChild(newli);
+
+  var span = document.createElement("SPAN");
+  var text = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(text);
+  newli.appendChild(span);
 }
 
-// Close item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
+let dl = document.querySelector('ul')
+dl.addEventListener('click', (del) => {
+  if (del.target.tagName === 'SPAN') {
+      let tdlist = document.querySelectorAll('.list-group-item').forEach(td => {
+          td.addEventListener('click', () => {
+              td.remove()
+          })
+      })
   }
-}
-
-// Checked
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
+})
