c7d9281bc64655b212fcf32927c58bec2bc9721b
Jeerasak Phalee <76503403+Jeerasak07@users.noreply.github.com>
2022-07-24 09:30:31 +0700

    create

--- Todolist/indexdom.js
+++ Todolist/indexdom.js
@@ -1,13 +1,6 @@
-//<h1 id="title" class="display-1" style="color:brown">Hello World</h1>
-//<h1 id="title2" class="display-2" style="color:brown">I are developer</h1>
-
 let h1 = document.getElementById('title');
 let h2 = document.getElementsByTagName('h1');
 let h3 = document.getElementsByClassName('display-3');
-
-//h1.innerText = "Computer and Information Science";
-//console.log(h2);
-//h2[0].innerHTML = "<b>CIS</b>";
 
 function changetext(id){
     console.log(id);
